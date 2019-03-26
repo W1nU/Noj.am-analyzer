d533db525e8bb2df1dea5b3ac537098aea4cb4a7
SEUNGWOO JUNG <jsw9808@gmail.com>
2019-03-26 15:24:00 +0900

    크롤링과 유저 정보 저장

---
+++ crawller.py
@@ -0,0 +1,20 @@
+import requests as r
+from bs4 import BeautifulSoup as bs
+
+class Crawller:
+    def __init__(self):
+        pass
+
+    def create_bs_object(self, html):
+        return bs(html, 'html.parser')
+
+    def get_info_from_id(self, id):
+        raw_html = r.get('https://www.acmicpc.net/user/' + id)
+        bs = self.create_bs_object(raw_html.text)
+        problems = list(map(lambda x : x.text, bs.find_all('span', {'class' : 'problem_number'})))
+        
+        return problems
+    
+
+cr = Crawller()
+cr.get_info_from_id('wjdtmddn98')
---
+++ user.py
@@ -0,0 +1,61 @@
+import os
+import pandas 
+
+class user:
+    def __init__(self):
+        if 'user.csv' not in os.listdir('./'):
+            with open('user.csv', 'w') as f:
+                csv.writer(f).writerow(['id', 'problems'])
+        
+        self.df = pandas.read_csv('./user.csv')
+
+    def check_user_exist(self, id):
+        if id not in list(self.df['id']):
+            return 0
+        
+        else:
+            return 1
+
+    def add_user(self, id, problems = ''):
+        if self.check_user_exist(id) == 1:
+            return 'UserDuplicateError'
+
+        self.df.loc[len(self.df)] = [id, problems]
+
+    def find_user_row(self, id):
+        if self.check_user_exist(id) == 0:
+            return 'UserNotExistError'
+        
+        else:
+            for n, i in enumerate(self.df['id']):
+                if i == id:
+                    return n
+        
+    def add_user_resolved_problems(self, id, problems):
+        if self.check_user_exist(id) == 0:
+            return 'UserNotExistError'
+        
+        else:
+            index = self.find_user_row(id)
+            self.df.iloc[index, 1] = self.make_resolved_problem_string(problems)
+
+    def make_resolved_problem_string(self, problems):
+        string = ''
+
+        for i in problems[:len(problems) - 1]:
+            string += f'{i} '
+        
+        return string + problems[len(problems) - 1]
+
+    def user_resolved_problems(self, id):
+        if self.check_user_exist(id) == 0:
+            return 'UserNotExistError'
+        
+        else:
+            index = self.find_user_row(id)
+            return self.df.iloc[index, 1] 
+
+    def save_csv(self):
+        self.df.to_csv('user.csv', index=False, header = True)
+
+        
